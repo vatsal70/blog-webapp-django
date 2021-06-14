52f0a68920f27354dfd558786f3404d8c9131d74
vatsal70 <68066033+vatsal70@users.noreply.github.com>
2021-06-14 17:53:03 +0530

    Update settings.py

--- webapp/settings.py
+++ webapp/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 
